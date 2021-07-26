6466543690fb5c370a1889a76cd759445c741cc7
Farce4wrd <74463978+Farce4wrd@users.noreply.github.com>
2021-07-26 13:07:15 -0400

    Add files via upload
    -Prep for angular connection
    -Work to be done: including api commands

--- MealPlanner/src/main/java/com/petero/eatsimple/config/DataConfig.java
+++ MealPlanner/src/main/java/com/petero/eatsimple/config/DataConfig.java
@@ -2,6 +2,7 @@ package com.petero.eatsimple.config;
 
 import java.io.IOException;
 
+
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- MealPlanner/src/main/java/com/petero/eatsimple/controllers/IngredientRestController.java
+++ MealPlanner/src/main/java/com/petero/eatsimple/controllers/IngredientRestController.java
@@ -3,9 +3,11 @@ package com.petero.eatsimple.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +40,18 @@ public class IngredientRestController {
 		return Mono.just("{}");
 	}
 	
+	@PutMapping(path = "{ingredientId}",  produces= MediaType.APPLICATION_JSON_UTF8_VALUE, consumes= MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Mono<String> updateCost(@PathVariable String ingredientId ,@RequestBody Mono<Ingredient> ingredient){
+		
+		return Mono.just("{}");
+	}
+	
+	@DeleteMapping(path = "{ingredientId}")
+	public Mono<Boolean> deleteIngredient(@PathVariable String ingredientId){
+		
+		return Mono.just(true);
+		
+	}
 
 
 }
