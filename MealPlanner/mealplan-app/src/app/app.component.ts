57b984c56bdb3e37ad33efba5cf3709a58fe0620
Peter Okolocha <74463978+Farce4wrd@users.noreply.github.com>
2021-07-31 14:09:23 -0400

    Introduced front-end assets
    -Included pictures
    -Included ingredient tab
    [To-do: Improve the ingredient section]

--- MealPlanner/mealplan-app/src/app/app.component.ts
+++ MealPlanner/mealplan-app/src/app/app.component.ts
@@ -7,6 +7,8 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'mealplan-app';
 
@@ -14,5 +16,28 @@ export class AppComponent {
 
   private baseUrl:string = 'http://localhost:8080';
   private reservationUrl:string = this.baseUrl + 'meal/ingredients/';
+
+  ingredients!: Ingredient[]
+
+  ngOnInit(){
+    this.ingredients = [ new Ingredient("101", "Rice", 30, 300),
+    new Ingredient("102", "Bread", 40, 250),
+    new Ingredient("103", "Veggies", 20, 150)];
+  }
+
+}
+
+export class Ingredient{
+  id: string;
+  name: string;
+  cost: number;
+  calories: number;
+
+  constructor(id: string, name: string, cost: number, calories: number){
+    this.id = id;
+    this.name = name;
+    this.cost = cost;
+    this.calories = calories;
+  }
 
 }
