1b98557a3d35c9d677df522e0fdcdc76d3d54286
Peter Okolocha <74463978+Farce4wrd@users.noreply.github.com>
2021-08-05 16:55:55 -0400

    Junit Testing (in-progress)
    -Still incomplete
    -Finish when at home

---
+++ MealPlanner/src/test/java/com/petero/eatsimple/controllers/IngredientRestControllerTest.java
@@ -0,0 +1,48 @@
+package com.petero.eatsimple.controllers;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.petero.eatsimple.models.Ingredient;
+import com.petero.eatsimple.models.Meal;
+
+import static com.petero.eatsimple.controllers.IngredientRestController.MEAL_INGREDIENTS;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IngredientRestControllerTest {
+	
+	@Autowired
+	private ApplicationContext context;
+	private WebTestClient webTestClient;
+	private Ingredient ingredient;
+	private Meal meal;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		webTestClient = WebTestClient.bindToApplicationContext(this.context).build();
+		
+		ingredient = new Ingredient("id", "Beans", 20, 200, meal);
+	}
+
+	@Test
+	void testGetAllIngredients() {
+		webTestClient.get().uri(MEAL_INGREDIENTS);
+	}
+
+	@Test
+	void testCreateIngredient() {
+		fail("Not yet implemented");
+	}
+
+}
